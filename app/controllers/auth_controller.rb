488de1fef605d9b021903c7ecf7245148d83c229
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-12-17 09:50:12 +0200

    added error messages if email or password is wrong to create action on auth controller, added route for reauth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,13 +3,13 @@ class AuthController < ApplicationController
 
   def create
     @user = User.find_by(email: user_login_params[:email])
-    
     if @user&.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
-   
+    elsif !@user 
+      render json: { error: "Please check your email" }, status: :unauthorized
     else
-      render json: { message: 'Invalid email or password' }, status: :unauthorized
+      render json: { error: "Please check your password" }, status: :unauthorized
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login', to: 'auth#create'
+  get '/reauth' , to: 'auth#reauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
