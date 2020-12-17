a8d77996b9fcbc47e14871d1dbdfee8470ccbbf4
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-12-17 09:34:56 +0200

    added appointments to render on user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-  byebug
+  
   
     if @user.valid?
       @token = encode_token({ user_id: @user.id })
