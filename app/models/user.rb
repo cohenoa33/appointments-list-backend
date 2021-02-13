1524b4a6e24c99701191e6a83eeca356a55e0feb
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-02-13 06:55:51 -0800

    added validations to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :appointments
 
-    validates :email, uniqueness: { case_sensitive: false }, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+ 
 
+
+    validates :email, uniqueness: { case_sensitive: true }, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x }
 
 end
