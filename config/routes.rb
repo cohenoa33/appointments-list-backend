10aeb619147540db86d5c31b265c33a4c4e709d3
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-02-13 07:12:17 -0800

    welcome controler and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'auth#create'
   get '/reauth' , to: 'auth#reauth'
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
